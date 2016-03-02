33eaf3ead775bad07500b3e7f1494fc4fa548f9c
Netanel Draiman <ndraiman@gmail.com>
2016-03-02 16:43:40 +0200

    added edit button and checkbox for list items

--- js/controllers/goalsIndexController.js
+++ js/controllers/goalsIndexController.js
@@ -9,51 +9,86 @@ angular.module('ProgressReport')
         console.log("Generating Mock Data");
 
         DatabaseService.clearGoals();
-        
+        localStorage.setItem('categories', JSON.stringify(['cat1', 'cat2']));
+
         var numOfMocks = 50;
 
         for (var i = 0; i < numOfMocks; i++) {
-            var r = Math.pow(Math.random()*numOfMocks, 2).toFixed(0);
+            var r = Math.pow(Math.random() * numOfMocks, 2).toFixed(0);
             var start = new Date(2016, 0, 3);
-            var end = new Date(2017,5,31);
+            var end = new Date(2017, 5, 31);
             var date = new Date(+start + Math.random() * (end - start)).toLocaleDateString();
             var category = Math.random() < 0.5 ? 'cat1' : 'cat2';
-            
+
             var mockObject = {
                 title: 'Title ' + r,
                 description: 'Description ' + i,
                 icon: 'assignment',
-                progress: (Math.random()*100).toFixed(0),
+                progress: (Math.random() * 100).toFixed(0),
                 date: date,
                 category: category
             };
             DatabaseService.addGoal(mockObject);
         }
-        
+
         $scope.goals = DatabaseService.getAllGoals();
     };
-    
-    $scope.mockDelete = function() {
+
+    $scope.mockDelete = function () {
         DatabaseService.clearGoals();
         $scope.goals = DatabaseService.getAllGoals();
     };
-    
-    
+
+
     /**************************************/
     /********** MOCK METHODS END **********/
     /**************************************/
-    
+
     $scope.search = {};
-//    $scope.activeCategory = {};
+    $scope.goals = DatabaseService.getAllGoals();
     $scope.categories = DatabaseService.getCategories();
+    $scope.checkedGoals = [];
+    $scope.editMode = false;
+
+
+    /****************************/
+    /******* Click Methods ******/
+    /****************************/
+    $scope.goalClicked = function (goal) {
+        if (!$scope.editMode) {
+            $scope.navigateTo('/goals/' + goal.title);
+        } else {
+            $scope.checkGoal(goal);
+        }
+    };
 
     $scope.navigateTo = function (path) {
         $location.path(path);
     };
 
-    $scope.goals = DatabaseService.getAllGoals();
+    /****************************/
+    /***** Edit Mode Methods ****/
+    /****************************/
+    $scope.checkGoal = function (goal) {
+        var checked = $scope.checkedGoals.indexOf(goal);
+        
+        if(checked > -1) {
+            $scope.checkedGoals.splice(checked, 1);
+        } else {
+            $scope.checkedGoals.push(goal);
+        }
+    };
+    $scope.toggleEditMode = function () {
+        $scope.checkedGoals = [];
+        $scope.editMode = !$scope.editMode;
+    };
+    $scope.isChecked = function (goal) {
+        return $scope.checkedGoals.indexOf(goal) > -1;
+    };
 
-
+    /****************************/
+    /******* Dialog Method ******/
+    /****************************/
     $scope.addDialog = function ($event) {
         var useFullScreen = $mdMedia('sm') || $mdMedia('xs');
 
