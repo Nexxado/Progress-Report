4b9632497e1f91fefb0ae05dada619e728263101
Netanel Draiman <ndraiman@gmail.com>
2016-03-09 12:38:26 +0200

    added basic home page

--- js/controllers/homePageController.js
+++ js/controllers/homePageController.js
@@ -1,7 +1,65 @@
 angular.module('ProgressReport')
 
-.controller('HomePageController', function ($scope) {
-    
+.controller('HomePageController', function ($scope, $timeout) {
+    $scope.pageClass = 'no-padding slide-right';
     $scope.goalsLink = '/#/goals';
-    
+
+
+    $timeout(function () {
+
+        var ctx = $(".graph").get(0).getContext("2d");
+
+        var data = {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    label: "My First dataset",
+                    fillColor: "rgba(220,220,220,0.2)",
+                    strokeColor: "rgba(220,220,220,1)",
+                    pointColor: "rgba(220,220,220,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                },
+                {
+                    label: "My Second dataset",
+                    fillColor: "rgba(151,187,205,0.2)",
+                    strokeColor: "rgba(151,187,205,1)",
+                    pointColor: "rgba(151,187,205,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(151,187,205,1)",
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        };
+
+        new Chart(ctx).Line(data, {});
+
+        ctx = $(".graph").get(1).getContext("2d");
+        data = [
+            {
+                value: 300,
+                color: "#F7464A",
+                highlight: "#FF5A5E",
+                label: "Red"
+            },
+            {
+                value: 50,
+                color: "#46BFBD",
+                highlight: "#5AD3D1",
+                label: "Green"
+            },
+            {
+                value: 100,
+                color: "#FDB45C",
+                highlight: "#FFC870",
+                label: "Yellow"
+            }
+        ];
+
+        new Chart(ctx).Pie(data, {});
+
+    });
 });
