73f38d5b9e21723fbeed24892fef17593cc4b6e4
Dan Draiman <DDraiman1990@gmail.com>
2016-03-27 23:00:15 +0200

    tried adding another type of slideshow - failed

--- js/controllers/howToUsePageController.js
+++ js/controllers/howToUsePageController.js
@@ -1,17 +1,18 @@
 angular.module('ProgressReport')
 
 .controller('HowToUsePageController', function ($scope) {
-//    $scope.pageClass = 'bounce-top'; //used for animation of ng-view
-    
-    $scope.slides = [
-        {
-            name: 'Not my cat.',
-            url: 'img/howToUse/01.png'
-        },
-        {
-            name: 'Again, not my cat.',
-            url: 'img/howToUse/02.png'
-        }]
+
+    // Scroll to appropriate position based on image index and width
+        ////    $scope.pageClass = 'bounce-top'; //used for animation of ng-view
+        //    
+        //    $scope.slides = [
+        //        {
+        //            name: 'Not my cat.',
+        //            url: 'img/howToUse/01.png'
+        //        },
+        //        {
+        //            name: 'Again, not my cat.',
+        //            url: 'img/howToUse/02.png'
+        //        }];
 
 });
-
